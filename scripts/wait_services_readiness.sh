343492bb3a89f236a5b9164bc51b95cc827addc2
antoinefa <fabien.antoine@m4x.org>
2022-01-17 21:50:08 +0100

    handles mongo being ready first in local judilibre-prive deployments

--- scripts/wait_services_readiness.sh
+++ scripts/wait_services_readiness.sh
@@ -3,27 +3,31 @@
 
 if [ "${APP_GROUP}" == "judilibre-prive" ]; then
         if [ "${KUBE_ZONE}" == "local" ];then
-                APP_DB=mongodb-0;
-        fi;
+                POD_ONE=mongodb-0;
+                POD_TWO=${APP_ID}-deployment
+        else
+                POD_ONE=${APP_ID}-deployment
+        fi
 else
-        APP_DB=${APP_GROUP}-es;
+        POD_ONE=${APP_ID}-deployment
+        POD_TWO=${APP_GROUP}-es;
 fi
 
 : ${START_TIMEOUT:=60}
 timeout=${START_TIMEOUT} ;
-for POD in ${APP_ID} ${APP_DB}; do
+for POD in ${POD_ONE} ${POD_TWO}; do
     ret=1 ;\
     until [ "$timeout" -le 0 -o "$ret" -eq "0" ] ; do\
-            status=$(${KUBECTL} get pod --namespace=${KUBE_NAMESPACE} | grep ${POD} | grep -v job | awk '{print $2}');
+            status=$(${KUBECTL} get pod --namespace=${KUBE_NAMESPACE} | grep ${POD} | awk '{print $2}');
             (echo $status | egrep -v '0/1|0/2|1/2' | egrep -q '1/1|2/2' );\
             ret=$? ; \
-            if [ "$ret" -ne "0" ] ; then printf "\r\033[2K%03d Wait for service ${POD} to be ready" $timeout ; fi ;\
+            if [ "$ret" -ne "0" ] ; then printf "\r\033[2K%03d Wait for pod ${POD} to be ready" $timeout ; fi ;\
             ((timeout--)); sleep 1 ; \
     done ;
 done;
 
 if [ "$ret" -ne "0" ]; then
-        echo -en "\r\033[2K\e[31m❌  all service are not ready !\e[0m\n";
+        echo -en "\r\033[2K\e[31m❌  all pods are not ready !\e[0m\n";
         ${KUBECTL} get pod --namespace=${KUBE_NAMESPACE};
         exit 1;
 else
