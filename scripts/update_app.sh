266476bb3be1e64998fd65fc05d8c9db77d946f6
Fabien Antoine <fabien.antoine@m4x.org>
2022-01-27 20:36:02 +0100

    Handles logs in update_app

--- scripts/update_app.sh
+++ scripts/update_app.sh
@@ -14,8 +14,28 @@ if [ -z "${KUBE_NAMESPACE}" ]; then
         export KUBE_NAMESPACE=${APP_GROUP}-${KUBE_ZONE}-$(echo ${GIT_BRANCH} | tr '/' '-')
 fi;
 
+# logs
+if [ -z "${KUBE_INSTALL_LOG}" ];then
+        export KUBE_INSTALL_LOG=$(pwd)/k8s-$(date +%Y%m%d_%H%M).log;
+fi
+
 #perform update
-kubectl set image --namespace=${KUBE_NAMESPACE} deployments/${APP_ID}-deployment ${APP_ID}=${DOCKER_USERNAME}/${APP_ID}:${VERSION}
+if (kubectl set image --namespace=${KUBE_NAMESPACE} deployments/${APP_ID}-deployment ${APP_ID}=${DOCKER_USERNAME}/${APP_ID}:${VERSION} > ${KUBE_INSTALL_LOG} 2>&1); then
+        echo "🚀 update ${APP_ID} reference to ${VERSION}";
+else
+        echo -e "\e[31m❌  update ${APP_ID} reference to ${VERSION}\e[0m" && exit 1;
+fi
 
 #test or rollback
-( ./scripts/wait_services_readiness.sh && ./scripts/test_minimal.sh ) || (kubectl -n ${KUBE_NAMESPACE} rollout undo deployments/${APP_ID}-deployment && exit 1)
+if ( ( ./scripts/wait_services_readiness.sh && ./scripts/test_minimal.sh ) > ${KUBE_INSTALL_LOG} 2>&1); then  
+        echo "✅  ${APP_ID}:${VERSION} upgrade";
+else
+        echo -e "\e[31m❌  ${APP_ID}:${VERSION} upgrade failed\e[0m";
+        if (kubectl -n ${KUBE_NAMESPACE} rollout undo deployments/${APP_ID}-deployment > ${KUBE_INSTALL_LOG} 2>&1); then
+                echo "✅  ${APP_ID} rollback successfull";
+                exit 1;
+        else
+                echo -e "\e[31m❌  ${APP_ID} rollback failed\e[0m";
+                exit 1;
+        fi;
+fi;
