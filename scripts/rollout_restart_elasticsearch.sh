0bb00201f078010755214df1697a6b63ec6d2da7
antoinefa <fabien.antoine@m4x.org>
2022-08-22 17:01:00 +0200

    add rollout restart elasticsearch for easier ES problem maintenance

---
+++ scripts/rollout_restart_elasticsearch.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+sudo echo -n
+
+./scripts/check_install.sh
+
+if [ -z "${KUBE_INSTALL_LOG}" ];then
+    export KUBE_INSTALL_LOG=$(pwd)/k8s-$(date +%Y%m%d_%H%M).log;
+fi;
+
+if [ -z "${KUBECTL}" ];then
+    export KUBECTL=$(which kubectl);
+fi
+
+#default k8s namespace
+if [ -z "${KUBE_NAMESPACE}" ]; then
+        export KUBE_NAMESPACE=${APP_GROUP}-${KUBE_ZONE}-$(echo ${GIT_BRANCH} | tr '/' '-')
+fi;
+
+for (( i=1 ; ((i-${ELASTIC_NODES})) ; i=(($i+1)) ));
+        do ${KUBECTL} -n ${KUBE_NAMESPACE} delete pod/${APP_GROUP}-es-default-${i};
+        ./scripts/wait_services_readiness.sh;
+done;
+echo
