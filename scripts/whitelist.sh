b9295f885630c11dcf68b9b09d0b60c3ec2fd1df
Fabien Antoine <fabien.antoine@m4x.org>
2022-10-24 08:31:39 -0400

    Update whitelist.sh
    Add alternative mj ip

--- scripts/whitelist.sh
+++ scripts/whitelist.sh
@@ -5,7 +5,7 @@
     curl -s https://uptimerobot.com/inc/files/ips/IPv4andIPv6.txt | grep -v '::' | tr '\n' ',' #uptime-robot
     curl -s https://updown.io/api/nodes/ipv4 | jq -r '.[]' | tr '\n' ',' #updown.io
     echo 185.24.185.46/27 #piste
-    echo 185.24.186.214,185.24.187.214 #mj
+    echo 185.24.186.214,185.24.187.214,185.24.184.214 #mj
     echo 51.15.223.35,174.89.249.181,91.170.21.111 #devops
     echo 80.87.224.0/22 #actimage
     echo 80.87.226.0/22 #new_actimage
