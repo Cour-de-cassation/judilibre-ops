32f01cffdfabcfe8104cf024c6b4fb3aa6b435ed
Sébastien Courvoisier <sebastien.courvoisier@justice.fr>
2024-09-12 09:10:42 +0200

    Update whitelist.sh

--- scripts/whitelist.sh
+++ scripts/whitelist.sh
@@ -8,6 +8,7 @@
     echo 185.24.186.254,185.24.186.214,185.24.187.214,185.24.184.214 #mj
     echo 51.15.223.35,174.89.249.181,91.170.21.111,51.255.99.129,51.77.234.17 #devops/piste
     echo 51.254.32.166 #devops/probe
+    echo 88.180.104.201 #dev/test
     echo 80.87.224.0/22 #actimage
     echo 80.87.226.0/22 #new_actimage
     echo 80.87.225.11 #actimage_supervision
