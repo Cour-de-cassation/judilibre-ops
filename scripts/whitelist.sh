ce2ec9a7499d26c6c244db1b7128b8c11dfe964a
Ilias Aaguida <ilias.aaguida@hotmail.fr>
2024-10-30 17:32:40 +0100

    update whitelist

--- scripts/whitelist.sh
+++ scripts/whitelist.sh
@@ -12,6 +12,11 @@
     echo 80.87.224.0/22 #actimage
     echo 80.87.226.0/22 #new_actimage
     echo 80.87.225.11 #actimage_supervision
+    # new_mj
+    echo 143.126.250.12/32,143.126.250.13/32,143.126.250.14/32,143.126.250.15/32 # Secours 
+    echo 143.126.248.12/32,143.126.248.13/32,143.126.248.14/32,143.126.248.15/32 # Secours
+    echo 143.126.249.12/32,143.126.249.13/32,143.126.249.14/32,143.126.249.15/32 # Nominal
+    echo 143.126.251.12/32,143.126.251.13/32,143.126.251.14/32,143.126.251.15/32 # Secours
 ) | tr '\n' ',' | sed 's/,$//'
 
 # If you want to update the whitelist, update this file and deploy it by deploying judilibre-search in scaleway (with github action).
